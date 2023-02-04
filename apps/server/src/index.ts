b06b14e40ff14d018754837d181c3343f020b646
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2023-02-05 01:57:03 +0200

    Remove cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,16 +1,13 @@
-import cors from "cors";
 import express from "express";
 
 const PORT = 8080;
 
 const app = express();
 
-app.disable("x-powered-by");
 app.use(express.json());
-app.use(cors());
 
 app.get("/", (_req, res) => {
-  return res.json({ message: "Hello Team!" });
+  return res.status(200).json({ message: "Hello Team!" });
 });
 
 app.listen(PORT, () => {
