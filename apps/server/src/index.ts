75f7fe12c84a397cef5cfe3b6ab1a06fa5e14c70
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2023-02-06 23:50:41 +0200

    Get metadata from input

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,7 +2,10 @@ import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
 import { spawn } from "child_process";
 import cors from "cors";
 import express from "express";
+import fs from "fs";
 import helmet from "helmet";
+import path from "path";
+import { v4 as uuid } from "uuid";
 
 const PORT = process.env.PORT ?? 8080;
 
@@ -11,9 +14,21 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
-app.get("/", (_req, res) => {
-  spawn(ffmpegPath, [
+app.post("/api/add-metadata", (req, res) => {
+  const title = req.body.title as string;
+  const artist = req.body.artist as string;
+
+  if (!title || !artist) return res.status(400).json({ message: "Fill in required fields" });
+
+  const id = uuid();
+
+  if (!fs.existsSync("./uploads")) {
+    fs.mkdirSync("./uploads");
+  }
+
+  const child = spawn(ffmpegPath, [
     "-i",
     "./song.mp3",
     "-i",
@@ -27,13 +42,18 @@ app.get("/", (_req, res) => {
     "-id3v2_version",
     "3",
     "-metadata",
-    "title=How Could I Be Mad?",
+    `title=${title}`,
     "-metadata",
-    "artist=Glaive",
-    "./out.mp3",
+    `artist=${artist}`,
+    `./uploads/${id}.mp3`,
   ]);
 
-  res.status(200).json({ message: "Hello World!" });
+  return child.on("exit", (code) => {
+    if (code !== 0) {
+      return res.status(500).json({ message: "Something went wrong" });
+    }
+    return res.status(200).json({ file_path: `http://localhost:${PORT}/uploads/${id}.mp3` });
+  });
 });
 
 app.listen(PORT, () => {
