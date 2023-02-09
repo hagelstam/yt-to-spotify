2539f92b8b3c23c3af3cbbc031013ee6e058c768
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2023-02-09 22:35:08 +0200

    Add env variables

--- apps/server/src/env.d.ts
+++ apps/server/src/env.d.ts
@@ -2,6 +2,8 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     PORT: number;
     NODE_ENV: "development" | "production";
+    SERVER_URL: string;
+    CLIENT_URL: string;
   }
 }
 
--- apps/server/src/utils/constants.ts
+++ apps/server/src/utils/constants.ts
@@ -4,4 +4,6 @@ export const PORT = process.env.PORT ?? 8080;
 
 export const SERVER_URL = process.env.SERVER_URL ?? `http://localhost:${PORT}`;
 
+export const CLIENT_URL = process.env.CLIENT_URL ?? `http://localhost:3000`;
+
 export const FILES_PATH = path.join(__dirname, "../../uploads");
