80920dc47ad986eff839360012fd77fcc0016e5c
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-10-20 19:59:43 +0300

    Update deps

--- apps/server/src/utils/constants.ts
+++ apps/server/src/utils/constants.ts
@@ -1,9 +1,6 @@
 import path from "path";
 
 export const PORT = process.env.PORT ?? 8080;
-
 export const SERVER_URL = process.env.SERVER_URL ?? `http://localhost:${PORT}`;
-
 export const CLIENT_URL = process.env.CLIENT_URL ?? `http://localhost:3000`;
-
 export const FILES_PATH = path.join(__dirname, "../../uploads");
