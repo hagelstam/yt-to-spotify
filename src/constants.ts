e612f8b089b79a8cfe79f31664daaddc42936acf
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-01-26 18:24:27 +0200

    New folder for each request

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,4 @@ import path from 'path'
 
 export const PORT = Number(process.env.PORT) || 3000
 
-export const DUMP_PATH = path.join(__dirname, '../temp')
-
-export const COVER_FILE = `${DUMP_PATH}/cover.jpg`
-
-export const AUDIO_FILE = `${DUMP_PATH}/audio.mp3`
-
-export const CROPPED_COVER_FILE = `${DUMP_PATH}/cropped_cover.jpg`
-
-export const FINAL_FILE = `${DUMP_PATH}/final.mp3`
+export const FILES_PATH = path.join(__dirname, '../files')
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
+import { randomUUID } from 'crypto'
 import express from 'express'
 import fs from 'fs'
 import path from 'path'
-import {
-  AUDIO_FILE,
-  COVER_FILE,
-  CROPPED_COVER_FILE,
-  DUMP_PATH,
-  FINAL_FILE,
-  PORT,
-} from './constants'
+import { FILES_PATH, PORT } from './constants'
 import { limiter, validateBody } from './middleware'
 import { addMetadata, cropCover, downloadAudio, downloadCover } from './utils'
 
@@ -17,17 +11,25 @@ const app = express()
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.urlencoded({ extended: true }))
 
+if (!fs.existsSync(FILES_PATH)) {
+  fs.mkdirSync(FILES_PATH)
+}
+
 app.get('/', (_req, res) => {
   return res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
 app.post('/convert', limiter, validateBody, async (req, res) => {
+  const requestId = randomUUID()
+  const DUMP_PATH = `${FILES_PATH}/${requestId}`
+  const AUDIO_FILE = `${DUMP_PATH}/audio.mp3`
+  const COVER_FILE = `${DUMP_PATH}/cover.jpg`
+  const CROPPED_COVER_FILE = `${DUMP_PATH}/cropped_cover.jpg`
+  const FINAL_FILE = `${DUMP_PATH}/final.mp3`
+
   try {
     const { youtubeUrl, artistName, songTitle } = req.body
 
-    if (fs.existsSync(DUMP_PATH)) {
-      fs.rmSync(DUMP_PATH, { recursive: true })
-    }
     fs.mkdirSync(DUMP_PATH)
 
     await Promise.all([
@@ -47,9 +49,15 @@ app.post('/convert', limiter, validateBody, async (req, res) => {
 
     return res.download(FINAL_FILE, fileName, () => {
       console.info(`File sent: ${fileName}`)
+      fs.rmSync(DUMP_PATH, {
+        recursive: true,
+      })
     })
   } catch (err) {
     console.error(err instanceof Error ? err.message : err)
+    fs.rmSync(DUMP_PATH, {
+      recursive: true,
+    })
     return res.redirect('/')
   }
 })
